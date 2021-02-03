6284bd63e9203fc12a831c25459a779a953ff405
prerana1821 <prerananawar1@gmail.com>
2021-02-03 19:11:54 +0530

    Updated Readme

--- namestrom/src/App.js
+++ namestrom/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
 }
 
 export default App;
