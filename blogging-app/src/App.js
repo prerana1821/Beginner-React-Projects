3796fc3f995857392bc2efe4946e148dbaa14cd0
prerana1821 <prerananawar1@gmail.com>
2021-02-03 13:36:47 +0530

    Completed blogging app

--- blogging-app/src/App.js
+++ blogging-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from "./Create";
 import Search from "./Search";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -24,6 +25,9 @@ function App() {
             </Route>
             <Route path='/blogs/:id'>
               <BlogDetails/>
+            </Route>
+            <Route path='*'>
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- blogging-app/src/Search.js
+++ blogging-app/src/Search.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Search = () => {
+
+    const [name, setName] = useState('');
+    const { data: blogs, isPending, error } = useFetch('http://localhost:9000/blogs');
+    let blogg = blogs && blogs.filter(blog => blog.author === name);
     return (
-        <div>
+        <div className='search'>
             <h1>Searching...</h1>
+            <form>
+                <label>Search by Author:</label>
+                <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </form>
+
+            {error && error}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogg}
+                title={name && `${name}'s Blogs`} />}
         </div>
     )
 }
